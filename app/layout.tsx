80d71200255174ad68d1521c545c6b2d03d97297
Augustine Onuora <holyaustin@yahoo.com>
2025-07-05 13:27:45 +0100

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { Poppins } from "next/font/google";
+
+import '@styles/globals.css';
+
+import { ContextProvider } from '.';
+import ReactQueryProvider from './ReactQueryProvider';
+
+// const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'], display: 'swap', adjustFontFallback: false})
+
+const poppins = Poppins({
+  weight: ["300", "400", "500"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+// Websit Config
+export const metadata: Metadata = {
+  title: 'StorageSyncOrbit',
+  description: 'MultiCahin OnRamp',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      {/**<body className={`${poppins.className} ...`}> */}
+      <body className={inter.className}>
+        <ReactQueryProvider>
+          <ContextProvider>{children}</ContextProvider>
+        </ReactQueryProvider>
+      </body>
+    </html>
+  );
+}
