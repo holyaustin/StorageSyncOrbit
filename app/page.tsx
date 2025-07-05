3bfa48a0e50583a63062d47c1f2637fbd1b88572
Augustine Onuora <holyaustin@yahoo.com>
2025-07-05 20:18:06 +0100

    foter2

--- app/page.tsx
+++ app/page.tsx
@@ -9,13 +9,16 @@ export default function Home() {
       <Header />
       <div>
         <div
-          className="relative w-full pt-48 pb-40 m-auto flex justify-center text-center flex-col items-center z-1 text-white"
+          className="relative w-full pt-24 pb-20 m-auto flex justify-center text-center flex-col items-center z-1 text-white"
           style={{ maxWidth: '1200px' }}
         >
-          <h1 className="inline-block max-w-2xl lg:max-w-4xl  w-auto relative text-5xl md:text-6xl lg:text-7xl tracking-tighter mb-10 font-bold">
-            StorageSyncOrbit : A cross-chain data bridge protocol for decentralized storage on Filecoin{' '}
+          <h1 className="inline-block max-w-2xl lg:max-w-4xl text-pink-300 w-auto relative text-5xl md:text-6xl lg:text-7xl tracking-tighter mb-10 font-bold">
+            StorageSyncOrbit
           </h1>
-          <p className="text-2xl mb-5">Orbiting across blockchains to unify data storage</p>
+          <h1 className="inline-block max-w-2xl lg:max-w-4xl  w-auto relative text-5xl md:text-6xl lg:text-7xl tracking-tighter mb-10 font-bold">
+            A cross-chain data bridge protocol for decentralized storage on Filecoin{' '}
+          </h1>
+          <p className="text-2xl mb-5">Orbiting across blockchains to unify data storage.</p>
           <WalletConnect />
         </div>
       </div>
--- components/footer.tsx
+++ components/footer.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image';
 
 export default function Footer() {
   return (
-    <div className="fixed bottom-0 w-full bg-blue-600 flex items-center flex-col lg:flex-row lg:justify-between gap-4 px-4">
-      <div className="flex flex-row gap-8 items-center justify-center mb-4">
+    <div className="fixed bottom-0 w-full bg-purple-950 flex items-center flex-col lg:flex-row lg:justify-between gap-4 px-4">
+      <div className="flex flex-row gap-8 items-center justify-center mb-4 pt-4">
         <a
           className="text-black items-center inline-flex bg-white border-2 border-black duration-200 ease-in-out focus:outline-none hover:bg-black hover:shadow-none hover:text-white justify-center rounded-full shadow-[5px_5px_black] text-center transform transition w-full px-2 py-2"
           href="https://x.com/holyaustin"
